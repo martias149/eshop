9b013b56a05b3aa3ce80e6182b6402b2f64f9973
martias149 <martias@seznam.cz>
2026-07-02 18:52:38 +0200

    Add HTML/JS frontend for product listing

--- eshop/settings.py
+++ eshop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'frontend'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'frontend']
