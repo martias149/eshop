c24514d034a1676758f15407c896b191de88ffe6
martias149 <martias@seznam.cz>
2026-07-02 19:02:34 +0200

    Add drf-spectacular Swagger UI at /api/docs/

--- eshop/urls.py
+++ eshop/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
